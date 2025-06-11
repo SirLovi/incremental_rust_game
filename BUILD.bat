4064d2d7962e0a0eff944258fe4cf2bd352fe7df
SirLovi <20193391+SirLovi@users.noreply.github.com>
2025-06-11 22:49:29 +0200

    Update BUILD.bat

--- BUILD.bat
+++ BUILD.bat
@@ -1,3 +1,7 @@
+rustup target add wasm32-unknown-unknown
+cargo install wasm-bindgen-cli
+
 cargo build --target wasm32-unknown-unknown
 wasm-bindgen target/wasm32-unknown-unknown/debug/incremental_rust_game.wasm --out-dir ./pkg --web
-python -m http.server
+
+python -m http.server 8000
