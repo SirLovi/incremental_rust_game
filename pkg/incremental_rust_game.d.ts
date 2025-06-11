9be61680b632dcf4d0e7a3cd62c7088932cd1563
Dominik Staněk <20193391+SirLovi@users.noreply.github.com>
2025-06-11 23:08:33 +0200

    embed wasm as base64 (#23)

---
+++ pkg/incremental_rust_game.d.ts
@@ -0,0 +1,108 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * JS facing game API
+ */
+export class Game {
+  free(): void;
+  /**
+   * Create a new game instance
+   */
+  constructor();
+  /**
+   * Advance the game state based on the current timestamp (seconds)
+   */
+  static tick(now: number): void;
+  /**
+   * Attempt to build a building by name
+   */
+  static build(name: string): boolean;
+  /**
+   * Get a resource amount by name
+   */
+  static get_resource(name: string): number;
+  /**
+   * Get the cost of constructing the next level of a building as a JSON string
+   */
+  static building_cost(name: string): string;
+  /**
+   * Save game to a base64 string
+   */
+  static save(): string;
+  /**
+   * Load game from a base64 string
+   */
+  static load(data: string): void;
+  /**
+   * Change tick rate in seconds
+   */
+  static set_tick_rate(rate: number): void;
+  /**
+   * Retrieve the next log message from the game if available
+   */
+  static pop_log(): string | undefined;
+  /**
+   * Get the list of unlocked achievements as a JSON string
+   */
+  static achievements(): string;
+  /**
+   * Attempt to research a technology using science
+   */
+  static research(name: string): boolean;
+  /**
+   * Perform a prestige reset
+   */
+  static prestige(): void;
+  /**
+   * Current prestige points
+   */
+  static prestige_points(): number;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_game_free: (a: number, b: number) => void;
+  readonly game_new: () => number;
+  readonly game_build: (a: number, b: number) => number;
+  readonly game_get_resource: (a: number, b: number) => number;
+  readonly game_building_cost: (a: number, b: number) => [number, number];
+  readonly game_save: () => [number, number];
+  readonly game_load: (a: number, b: number) => void;
+  readonly game_pop_log: () => [number, number];
+  readonly game_achievements: () => [number, number];
+  readonly game_research: (a: number, b: number) => number;
+  readonly game_prestige_points: () => number;
+  readonly game_set_tick_rate: (a: number) => void;
+  readonly game_tick: (a: number) => void;
+  readonly game_prestige: () => void;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __externref_table_alloc: () => number;
+  readonly __wbindgen_export_2: WebAssembly.Table;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ pkg/incremental_rust_game_bg.wasm.d.ts
@@ -0,0 +1,24 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_game_free: (a: number, b: number) => void;
+export const game_new: () => number;
+export const game_build: (a: number, b: number) => number;
+export const game_get_resource: (a: number, b: number) => number;
+export const game_building_cost: (a: number, b: number) => [number, number];
+export const game_save: () => [number, number];
+export const game_load: (a: number, b: number) => void;
+export const game_pop_log: () => [number, number];
+export const game_achievements: () => [number, number];
+export const game_research: (a: number, b: number) => number;
+export const game_prestige_points: () => number;
+export const game_set_tick_rate: (a: number) => void;
+export const game_tick: (a: number) => void;
+export const game_prestige: () => void;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
